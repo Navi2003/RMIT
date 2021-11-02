bb6f1adf9fc9e72db75a34ff5a76a01e5eab2c86
Navi2003 <thienphucdoantran@gmail.com>
2021-11-02 15:43:33 +0700

    Add files via upload

--- Engineer Computing 1/Week2/SumOfDigits.c
+++ Engineer Computing 1/Week2/SumOfDigits.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    int num, d1, d2, d3, sum;
+    int num, sum;
     printf("Enter a 3-digit number: ");
     scanf("%d",&num);
-    d1 = num % 100 % 10;
-    d2 = (num - d1) % 100 / 10;
-    d3 = (num - d1 - d2 * 10) / 100;
-    sum = d1 + d2 + d3;
+    sum = num / 100 + num % 100 / 10 + num % 100 % 10;
     printf("Sum of 3 digits: %d",sum);
     return 0;
 }
