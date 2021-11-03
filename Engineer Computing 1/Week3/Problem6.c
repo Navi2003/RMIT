46606f2bcf89001b255e622825de63b86b115249
Navi2003 <thienphucdoantran@gmail.com>
2021-11-03 15:54:07 +0700

    Add files via upload

--- Engineer Computing 1/Week3/Problem6.c
+++ Engineer Computing 1/Week3/Problem6.c
@@ -12,14 +12,14 @@ int main(){
     //Nested loop to create a pyramid, highest value increasing each loop
     for (row=1;row<=num;row++) {
         //Loop for printing blank spaces, decreasing each loop
-        for(space=num-row;space>=1;space--) {
+        for(space=num-row;space>0;space--) {
             printf("   ");
         }
-        //Loop for printing highest value to one, highest value increasing each loop
+        //Loop for printing highest value to one
         for(value=row;value>1;value--) {
             printf("%3d",value);
         }
-        //Loop for printing 2 to highest value, highest value increasing each loop
+        //Loop for printing 2 to highest value
         for(value;value<=row;value++) {
             printf("%3d",value);
         }
