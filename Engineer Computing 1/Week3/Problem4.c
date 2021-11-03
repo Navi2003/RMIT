6c64454dd063fe5b832e6f743e997979689fe56a
Navi2003 <thienphucdoantran@gmail.com>
2021-11-03 15:23:55 +0700

    Add files via upload

--- Engineer Computing 1/Week3/Problem4.c
+++ Engineer Computing 1/Week3/Problem4.c
@@ -10,9 +10,7 @@ int main() {
     //Loop for creating new row
     for (row = 1;row<=10;row++) {
         //Print out the row numbers for each row
-        if (column == 0) {
             printf("%2d |",row);
-        }
         //Loop for printing out the product of column and row
         for (column = 1;column <= 10;column++) {
                 printf("%4d",column * row);
