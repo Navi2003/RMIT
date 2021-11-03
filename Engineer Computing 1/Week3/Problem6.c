24d17d6b4364449f71d35a270c71a91018637e67
Navi2003 <thienphucdoantran@gmail.com>
2021-11-03 15:42:41 +0700

    Add files via upload

--- Engineer Computing 1/Week3/Problem6.c
+++ Engineer Computing 1/Week3/Problem6.c
@@ -1,30 +1,28 @@
 #include <stdio.h>
 int main(){
     //Declare variables necessary
-    int a, b, c, d, e;
+    int a, b, c, d;
     
     //Enter an integer, repeat if not in specified range
     do {
         printf("Enter an integer between 1 and 15: ");
         scanf("%d",&a);
     } while(a<1 || a>15);
-    d=a;
     //Nested loop to create a pyramid, highest value increasing each loop
     for (b=1;b<=a;b++) {
         //Loop for printing blank spaces, decreasing each loop
-        for(c=d;c>1;c--) {
+        for(c=a-b;c>=1;c--) {
             printf("   ");
         }
         //Loop for printing highest value to one, highest value increasing each loop
-        for(e=b;e>1;e--) {
-            printf("%3d",e);
+        for(d=b;d>1;d--) {
+            printf("%3d",d);
         }
         //Loop for printing 2 to highest value, highest value increasing each loop
-        for(e;e<=b;e++) {
-            printf("%3d",e);
+        for(d;d<=b;d++) {
+            printf("%3d",d);
         }
     printf("\n");
-    d--;
     }
     return 0;
 }
