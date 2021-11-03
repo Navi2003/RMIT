741276bde5b7b0feda279276c21cccd9f2b11ae0
Navi2003 <thienphucdoantran@gmail.com>
2021-11-03 15:58:15 +0700

    Add files via upload

--- Engineer Computing 1/Week3/Problem7.c
+++ Engineer Computing 1/Week3/Problem7.c
@@ -14,42 +14,42 @@ int main(){
                 printf("Please select from the given choices!\n");
             }
         } while (user>2 || user<-1);
-        if (user!=-1){
-            int comp = rand() % 3; //Randomize computer choice
-            //Print user choice
-            switch (user){
-                case 2:
-                    printf("You chose Rock!\n");
-                    break;
-                case 1:
-                    printf("You chose Paper!\n");
-                    break;
-                case 0:
-                    printf("You chose Scissors!\n");
-                    break;
-            }
-            //Print computer choice
-            switch (comp){
-                case 2:
-                    printf("Computer chose Rock!\n");
-                    break;
-                case 1:
-                    printf("Computer chose Paper!\n");
-                    break;
-                case 0:
-                    printf("Computer chose Scissors!\n");
-                    break;
-            }
-            //Determine who won and print result
-            if (user==comp){
-                printf("It's a draw\n");
-            } else if(comp-user==1||comp-user==-2){
-                printf("You won!\n");
-            } else if(user-comp==1||user-comp==-2){
-                printf("You lost!\n");
-            }
-            printf("\n");
+        if (user==-1){
+            break;
         }
+        int comp = rand() % 3; //Randomize computer choice
+        //Print user choice
+        switch (user){
+            case 2:
+                printf("You chose Rock!\n");                    break;
+            case 1:
+                printf("You chose Paper!\n");
+                break;
+            case 0:
+                printf("You chose Scissors!\n");
+                break;
+        }
+        //Print computer choice
+        switch (comp){
+            case 2:
+                printf("Computer chose Rock!\n");
+                break;
+            case 1:
+                printf("Computer chose Paper!\n");
+                break;
+            case 0:
+                printf("Computer chose Scissors!\n");
+                break;
+        }
+        //Determine who won and print result
+        if (user==comp){
+            printf("It's a draw\n");
+        } else if(comp-user==1||comp-user==-2){
+                    printf("You won!\n");
+                } else if(user-comp==1||user-comp==-2){
+                        printf("You lost!\n");
+                }
+        printf("\n");
     } while (user!=-1);
     printf("Goodbye!");
     return 0;
